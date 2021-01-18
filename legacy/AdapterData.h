024f45e933a5752789cb79f086796ec7ec0c08a3
Iaroslav <IaroslavInternational@yandex.ru>
2021-01-18 22:14:13 +0300

    Добавьте файлы проекта.

---
+++ legacy/AdapterData.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "EngineWin.h"
+
+#include <d3d11.h>
+
+#pragma comment(lib, "d3d11.lib")
+
+#pragma comment(lib, "DXGI.lib")
+
+#include <vector>
+
+class AdapterData
+{
+public:
+	AdapterData(IDXGIAdapter* pAdapter);
+	IDXGIAdapter* pAdapter = nullptr;
+	DXGI_ADAPTER_DESC desc;
+};
+
+class AdapterReader
+{
+public:
+	static std::vector<AdapterData> GetAdapterData();
+private:
+	static std::vector<AdapterData> adapters;
+};
