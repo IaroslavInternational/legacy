77349efca81f2122d648ba81342d6d79e9932f17
Iaroslav <IaroslavInternational@yandex.ru>
2021-05-22 21:00:20 +0300

    Реализован бызовый функционал для класса Object.

--- legacy/Object.h
+++ legacy/Object.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include "Graphics.h"
+
 class Object
 {
 public:
-
+	Object(std::string name, DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 orientation);
+public:
+	std::string	GetName() const;
+	DirectX::XMFLOAT3 GetPosition() const;
+	DirectX::XMFLOAT3 GetOrientation() const;
+	void SetName(std::string name);
+	void SetPosition(DirectX::XMFLOAT3 position);
+	void SetOrientation(DirectX::XMFLOAT3 orientation);
 private:
-	
+	std::string name;
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT3 orientation;
 };
-
---
+++ legacy/VisibleObject.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Object.h"
+
+namespace Rgph
+{
+	class RenderGraph;
+}
+
+class VisibleObject : public Object
+{
+public:
+	VisibleObject(std::string name, DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 orientation, bool visibility);
+public:
+	virtual void LinkTechniques(Rgph::RenderGraph& rg) = 0;
+	virtual void Submit(size_t channels) = 0;
+	void SetVisibility(bool visibility);
+	bool IsVisible() const;
+private:
+	bool visibility;
+};
